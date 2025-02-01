e5b676cddc1ab3cd106198662834daafa7ca5d5a
Alex <oleksandrtaranenko239@gmail.com>
2025-02-01 20:58:04 +0200

    fix img save

--- user/models.py
+++ user/models.py
@@ -46,7 +46,7 @@ class UserManager(BaseUserManager):
 
 def image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+    filename = f"{slugify(instance.email)}-{uuid.uuid4()}{extension}"
 
     return os.path.join("uploads/img/", filename)
 
